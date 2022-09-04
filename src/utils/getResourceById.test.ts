d3c511d953de097646e1c9f6e9ffdde9b8b7ce02
Wilrosmi <will.r.smith31@gmail.com>
2022-09-04 19:23:48 +0100

    Change test to update for new database structure

--- src/utils/getResourceById.test.ts
+++ src/utils/getResourceById.test.ts
@@ -3,10 +3,10 @@ import { IResource } from "./types";
 
 test("Returns correct resource when given a valid id", () => {
   expect(getResourceById(testArrayOfResources, 2)).toEqual({
-    title: "2",
+    description: "2",
     type: "2",
     link: "2",
-    message: "2",
+    likes: 2,
     time: "2",
     id: 2,
   });
@@ -14,10 +14,10 @@ test("Returns correct resource when given a valid id", () => {
 
 test("Returns placeholder resource when given null id", () => {
   expect(getResourceById(testArrayOfResources, null)).toEqual({
-    title: "",
+    description: "",
     type: "",
     link: "",
-    message: "",
+    likes: 0,
     time: "",
     id: -1,
   });
@@ -25,26 +25,26 @@ test("Returns placeholder resource when given null id", () => {
 
 const testArrayOfResources: IResource[] = [
   {
-    title: "1",
+    description: "1",
     type: "1",
     link: "1",
-    message: "1",
+    likes: 1,
     time: "1",
     id: 1,
   },
   {
-    title: "2",
+    description: "2",
     type: "2",
     link: "2",
-    message: "2",
+    likes: 2,
     time: "2",
     id: 2,
   },
   {
-    title: "3",
+    description: "3",
     type: "3",
     link: "3",
-    message: "3",
+    likes: 3,
     time: "3",
     id: 3,
   },
